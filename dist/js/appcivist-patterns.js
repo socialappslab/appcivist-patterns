cba803cbf5eebbd422be42ff65079f784338eae0
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-11-25 14:26:00 -0300

    Update dist version

--- dist/js/appcivist-patterns.js
+++ dist/js/appcivist-patterns.js
@@ -1,4 +1,47 @@
-(function(appcvui, document, window) {
+(function(window, document) {
+  var appcvui = window.appcvui || {};
+
+  appcvui.Navigation = function(appContainerSelector, navSelector) {
+    this.initialize(appContainerSelector, navSelector);
+  };
+
+  p = appcvui.Navigation.prototype;
+
+  p.constructor = appcvui.Navigation;
+
+  p.initialize = function(appContainerSelector, navSelector) {
+    this.appEl = document.querySelector(appContainerSelector);
+    this.el = document.querySelector(navSelector);
+    this.toggle = this.el.querySelector('.button__nav_toggle');
+    this.toggle.addEventListener('click', this.toggleNav.bind(this));
+  };
+
+  p.toggleNav = function(e) {
+    this.appEl.classList.toggle('nav--active');
+    this.el.classList.toggle('nav--active');
+  };
+  window.appcvui = appcvui;
+}(window, document));
+;(function(appcvui, document, window) {
+
+  appcvui.Proposal = function(appContainerSelector) {
+    this.themeWidget;
+    this.initialize(appContainerSelector);
+  }
+
+  p = appcvui.Proposal.prototype;
+
+  p.constructor = appcvui.Proposal;
+
+  p.initialize = function(appContainerSelector) {
+
+    this.appEl = document.querySelector(appContainerSelector);
+    if( this.appEl == null ) return;
+
+  }
+
+}( window.appcvui =  window.appcvui || {}, document, window ));
+;(function(appcvui, document, window) {
 
   appcvui.Campaign = function(appContainerSelector) {
     this.themeWidget;
@@ -96,49 +139,6 @@
   }
 
 }( window.appcvui =  window.appcvui || {}, document, window ));
-;(function(window, document) {
-  var appcvui = window.appcvui || {};
-
-  appcvui.Navigation = function(appContainerSelector, navSelector) {
-    this.initialize(appContainerSelector, navSelector);
-  };
-
-  p = appcvui.Navigation.prototype;
-
-  p.constructor = appcvui.Navigation;
-
-  p.initialize = function(appContainerSelector, navSelector) {
-    this.appEl = document.querySelector(appContainerSelector);
-    this.el = document.querySelector(navSelector);
-    this.toggle = this.el.querySelector('.button__nav_toggle');
-    this.toggle.addEventListener('click', this.toggleNav.bind(this));
-  };
-
-  p.toggleNav = function(e) {
-    this.appEl.classList.toggle('nav--active');
-    this.el.classList.toggle('nav--active');
-  };
-  window.appcvui = appcvui;
-}(window, document));
-;(function(appcvui, document, window) {
-
-  appcvui.Proposal = function(appContainerSelector) {
-    this.themeWidget;
-    this.initialize(appContainerSelector);
-  }
-
-  p = appcvui.Proposal.prototype;
-
-  p.constructor = appcvui.Proposal;
-
-  p.initialize = function(appContainerSelector) {
-
-    this.appEl = document.querySelector(appContainerSelector);
-    if( this.appEl == null ) return;
-
-  }
-
-}( window.appcvui =  window.appcvui || {}, document, window ));
 ;(function(appcvui, document, window, vex) {
 
   appcvui.ThemeWidget = function(selector) {
@@ -189,8 +189,8 @@
   appcvui.initialize = function(){
     vex.defaultOptions.className = 'vex-theme-plain';
     appcvui.navigation = new appcvui.Navigation('.appcivist', '#appcivist__nav');
-    if( document.querySelector('.appmain > .campaign') != null ) {
-      appcvui.campaign = new appcvui.Campaign('.campaign');
+    if( document.querySelectorAll('.appmain .proposals_and_ideas') != null ) {
+      appcvui.campaign = new appcvui.Campaign('.proposals_and_ideas');
     }
   };
 
